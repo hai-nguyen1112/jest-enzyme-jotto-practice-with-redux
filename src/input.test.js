3fa72d53c8577a60074496c328374e112b4aca44
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-09-08 14:02:08 -0400

    add test cases for word has been guessed

--- src/input.test.js
+++ src/input.test.js
@@ -8,7 +8,7 @@ const setup = (initialState = {}) => {
   const store = storeFactory(initialState);
   const wrapper = shallow(<Input store={store} />)
     .dive()
-    .dive(); // To dive into the Input component. This way it will return the Input component only
+    .dive(); // To dive into the Input component. This way it will return the child Input component only from the Connected Higher Order Component.
   // console.log(wrapper.debug())
   return wrapper;
 };
@@ -36,9 +36,23 @@ describe('render', () => {
     });
   });
   describe('word has been guessed', () => {
-    test('renders component without error', () => {});
-    test('does not render input box', () => {});
-    test('does not render submit button', () => {});
+    let wrapper;
+    beforeEach(() => {
+      const initialState = { success: true };
+      wrapper = setup(initialState);
+    });
+    test('renders component without error', () => {
+      const component = findByTestAttr(wrapper, 'component-input');
+      expect(component.length).toBe(1);
+    });
+    test('does not render input box', () => {
+      const inputBox = findByTestAttr(wrapper, 'input-box');
+      expect(inputBox.length).toBe(0);
+    });
+    test('does not render submit button', () => {
+      const submitButton = findByTestAttr(wrapper, 'submit-button');
+      expect(submitButton.length).toBe(0);
+    });
   });
 });
 
