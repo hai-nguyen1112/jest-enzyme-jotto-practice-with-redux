02126a8813eff298643f87d41d641f95eeea3792
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-09-09 11:57:57 -0400

    Install redux thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,7 +1,8 @@
 import checkPropTypes from 'check-prop-types';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import rootReducer from '../src/reducers';
+import { middlewares } from '../src/configureStore';
 
 export const findByTestAttr = (wrapper, val) => {
   return wrapper.find(`[data-test="${val}"]`);
@@ -18,5 +19,8 @@ export const checkProps = (component, conformingProps) => {
 };
 
 export const storeFactory = (initialState) => {
-  return createStore(rootReducer, initialState);
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+    createStore
+  );
+  return createStoreWithMiddleware(rootReducer, initialState);
 };
