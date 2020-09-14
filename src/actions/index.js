3505e297478356f486f0086ade8a2f2cbe929a28
hai-nguyen1112 <hainguyen871112@gmail.com>
2020-09-14 16:59:35 -0400

    Fixex getSecretWordDispatch for challenge 2

--- src/actions/index.js
+++ src/actions/index.js
@@ -26,19 +26,24 @@ export const guessWord = (guessedWord) => {
 };
 
 export const getSecretWord = () => {
-  return (dispatch) => {
-    return axios.get('http://localhost:3030').then((response) => {
-      dispatch({
-        type: actionTypes.SET_SECRET_WORD,
-        payload: response.data,
-      });
+  return getSecretWordDispatch;
+};
+
+const getSecretWordDispatch = (dispatch) => {
+  return axios.get('http://localhost:3030').then((response) => {
+    dispatch({
+      type: actionTypes.SET_SECRET_WORD,
+      payload: response.data,
     });
-  };
+  });
 };
 
 export const resetGame = () => {
-  return {
-    type: actionTypes.RESET_GAME,
+  return (dispatch) => {
+    dispatch({
+      type: actionTypes.RESET_GAME,
+    });
+    return getSecretWordDispatch(dispatch);
   };
 };
 
